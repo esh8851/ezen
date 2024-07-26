28f6d867b350f8aebf3512d7098998102fb20ae3
esh8851 <esh8851@gmail.com>
2024-07-26 11:10:47 +0900

    ch02

---
+++ src/com/mycompany/ch02/BooleanEx.java
@@ -0,0 +1,17 @@
+package com.mycompany.ch02;
+
+public class BooleanEx {
+
+	public static void main(String[] args) {
+		
+		boolean isStop = true; // 관례적으로 boolean 타입의 변수명 앞에는 is 를 붙인다
+		
+		if(isStop) {
+			System.out.println("true");
+		} else {
+			System.out.println("false");
+		}
+		
+	}
+	
+}
---
+++ src/com/mycompany/ch02/LongEx.java
@@ -0,0 +1,13 @@
+package com.mycompany.ch02;
+
+public class LongEx {
+
+	public static void main(String[] args) {
+		
+		long var = 10000000000l; // int 범위를 벗어나면 L 을 붙여줘야 한다
+		
+		System.out.println(var);
+		
+	}
+
+}
